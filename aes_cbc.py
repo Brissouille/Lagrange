a54b193c01b78f737d91b874ce15eab2e4b0a0a5
Brissouille <valliere.brice@gmail.com>
2019-06-21 19:10:58 +0200

    Implementation of the Aes_Cbc, not tested completly

--- aes_cbc.py
+++ aes_cbc.py
@@ -4,9 +4,9 @@ from .model import Model
 
 class Aes_Cbc():
     def __init__(self, keylength, size_message): 
-        # The message must be a mutliple of 32 bytes
-        assert((size_message!=0)%32)
-        self.blocks = size_message // 32
+        # The message must be a mutliple of 16 bytes
+        assert((size_message!=0)%16)
+        self.blocks = size_message // 16
         
         # Init message in CBC
         self.message = [0] * self.blocks
@@ -26,17 +26,8 @@ class Aes_Cbc():
 
         # Aes list
         self.aes = [0] * self.blocks
-        self.aes[0] = Aes(keylength, "m0")
               
-        # We init m0 in the Aes class by M0^iv in the Aes_Cbc class
-        for i in range(4):
-            for j in range(4):
-                self.s.add(self.aes[0].message[i][j] == self.message[0][i][j] ^ self.iv[i][j])
-        
-        # We add the equations of Aes solver into Aes_cbc solver
-        #self.s.add(self.aes[0].s.assertions())
-
-        for b in range(1, self.blocks):
+        for b in range(0, self.blocks):
             # Init message of the Aes_Cbc class
             self.message[b] = [0] * 4
 
@@ -48,46 +39,65 @@ class Aes_Cbc():
                 self.message[b][i] = [0] * 4
                 for j in range(4):
                     self.message[b][i][j] = BitVec("M%02d_%02d_%02d" %(b, i, j), 8)
-                    self.s.add(self.aes[b].message[i][j] == self.message[b][i][j] ^ self.aes[b-1].message[i][j])
-            
-            # We add the equations of the ith Aes solver into Aes_cbc solver
-            #self.s.add(self.aes[b].s.assertions())
+       
+        self.reset()
 
     def check(self):
-        pass
+        self.s.check()
+        print("{:02x}".format(int(str(self.s.model().evaluate(self.aes[1].cipher[1][2])))))
+
+    def addIv(self, iv):
+        for i in range(4):
+            for j in range(4):
+                self.s.add(self.iv[i][j]==iv[2*(4*i+j):2*(4*i+j+1)])
+    
+    def addMessage(self, value):
+        for b in range(self.blocks):
+            for i in range(4):
+                for j in range(4):
+                    self.s.add(self.message[b][i][j]==int(value[2*(i*4+j):2*(i*4+j+1)],16))
 
     def encrypt(self, key, plaintext, iv):
         plain_len = len(plaintext)
 
         # We padd with zeroes bytes
-        padding = "0"+(32 - (plain_len % 32) % 32)
+        padding = "0"*(32 - (plain_len % 32) % 32)
         plaintext = plaintext + padding
 
+        # Init iv in solver
         self.addIv(iv)
 
-        Aes.masterkey = 
-
+        # Add message in the solver
         self.addMessage(plaintext)
-        # self.addMessage(b, j, i, plaintext[2*(i*4+j):2*(i*4+j+1)])
+
+        # Add the key in one aes -> all aes are impacted
+        for i in range(0, self.aes[0].Nk):
+            for j in range(0, 4):
+                self.aes[0].addPartialKey(i//4, i, j, int(key[2*(i*4+j):2*(i*4+j+1)], 16))
+
+        self.check()
 
     def decrypt(self, cipher):
         pass
 
     def resetSolver(self):
-        self.s.reset()
+        s = Solver()
+        s.reset()
         
         # Init only Sbox 
-        self.s.add(self.aes[0].s.assertions())
+        s.add(self.aes[0].s.assertions())
 
         for i in range(4):
             for j in range(4):
-                # We init the message with iv in the sovler
-                self.s.add(self.aes[0].message[i][j] == self.message[0][i][j] ^ self.iv[i][j])
-        
-                # We add all the assertions of the other system of equation
-                for b in range(1, self.blocks):
-                    self.aes[b].s.reset()
-                    self.s.add(self.aes[b].message[i][j] == self.aes[b].message[i][j] ^ self.message[b][i][j])
+                s.add(self.aes[0].message[i][j] == self.message[0][i][j] ^ self.iv[i][j])
+
+        # We add all the assertions of the other system of equation
+        for b in range(1, self.blocks):
+            self.aes[b].s.reset()
+            for i in range(4):
+                for j in range(4):
+                    s.add(self.aes[b].message[i][j] == self.message[b][i][j] ^ self.aes[b-1].cipher[i][j])
+        return s
             
     def reset(self):
         self.s = Aes_Cbc.resetSolver(self)
