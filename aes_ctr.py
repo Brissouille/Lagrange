662116395667d2d7d35819507c61610be4055a8b
Brissouille <valliere.brice@gmail.com>
2019-06-25 22:27:55 +0200

    Modification of the function init_mode of the Aes_Ctr class. Not finished

--- aes_ctr.py
+++ aes_ctr.py
@@ -6,28 +6,24 @@ class Aes_Ctr(Aes_Mode):
         super().__init__(keylength, size_message)
 
     def init_mode(self, s):
-        # We transform initial vector to an integer
-        iv_int = BV2Int(Concat([self.iv[i][j] for i in range(4) for j in range(4)]))
+        # We concatenate the initial vector for the addition. Concatenation is column order
+        iv_int = Concat([self.iv[i][j] for i in range(4) for j in range(4)])
         for b in range(0, self.blocks):
             # Init the block aes
             self.aes[b].s.reset()
             
             # Increment the iv in fonction of the block number
-            tmp = iv_int + b
-           
+            tmp = iv_int + 0
+            
             # Init the solver for CTR 
-            i = 3
-            j = 3
-            while(i>=0):
-                # Adding the equation
-                s.add(self.cipher[b][i][j] == self.aes[b].cipher[i][j] ^ self.message[b][i][j])
-                
-                # We add the Less Significant byte in the equation of the solver
-                a = tmp % 256
-                s.add(self.aes[b].message[i][j] == )
-
-                # For the next iteration
-                tmp = tmp / 256
-                j = (j - 1)%4
-                if(j==3):
-                    i = i - 1
+            for i in range(4):
+                for j in range(4):
+                    # Adding the equation
+                    s.add(self.cipher[b][i][j] == self.aes[b].cipher[i][j] ^ self.message[b][i][j])
+                  
+                    s.add(self.aes[b].message[i][j] == Extract((3-i)*4+(3-j)*8+7, ((3-i)*4+(3-j)-1)*8, tmp))
+    
+                    tmp = LShR(tmp, 8)
+                    j = (j - 1)%4
+                    if(j==3):
+                        i = i - 1
