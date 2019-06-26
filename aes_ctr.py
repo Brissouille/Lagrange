4f19a55fbecb848e0a771d7d3983cdea11f75f7a
Brissouille <valliere.brice@gmail.com>
2019-06-26 17:14:21 +0200

    Bug fixed for the counter mode, but to be tested

--- aes_ctr.py
+++ aes_ctr.py
@@ -8,13 +8,13 @@ class Aes_Ctr(Aes_Mode):
     def init_mode(self, s):
         # We concatenate the initial vector for the addition. Concatenation is column order
         iv_int = Concat([self.iv[i][j] for i in range(4) for j in range(4)])
-        print(iv_int)
+        
         for b in range(0, self.blocks):
-            # Init the block aes
-            self.aes[b].s.reset()
+            # Init the block aes ->  init causes the fail
+            #self.aes[b].s.reset()
             
             # Increment the iv in fonction of the block number
-            tmp = iv_int + 0
+            iv_tmp = iv_int + b
             
             # Init the solver for CTR 
             for i in range(4):
@@ -22,7 +22,8 @@ class Aes_Ctr(Aes_Mode):
                     indice_hi = ( ((3-i)*4 + (3-j) + 1) * 8 -1 )
                     indice_low = ( ((3-i)*4 + (3-j)) * 8 )
 
-                    # Adding the equation
-                    s.add(self.aes[b].message[i][j] == Extract(indice_hi, indice_low, tmp))
-                    s.add(self.cipher[b][i][j] == self.aes[b].cipher[i][j] ^ self.message[b][i][j])
+                    # Adding the iv in the solver
+                    s.add(self.aes[b].message[i][j] == simplify(Extract(indice_hi, indice_low, iv_tmp)))
 
+                    # Adding the xor between plain and encrypted iv in the solver
+                    s.add(self.cipher[b][i][j] == self.aes[b].cipher[i][j] ^ self.message[b][i][j])
