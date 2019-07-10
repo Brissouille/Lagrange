2cfa6b17d33451b497753b2d3315f3df9abb24f2
Brissouille <valliere.brice@gmail.com>
2019-07-10 17:46:22 +0200

    Development DFA classes not work

--- dfa.py
+++ dfa.py
@@ -3,22 +3,33 @@ from z3 import *
 
 class DFA():
     def __init__(self):
-        self.aes1 = Aes(128, "m1")
-        self.aes2 = Aes(128, "m2")
+        self.aes1 = Aes(128)
+        self.aes2 = Aes(128)
         self.s = Solver()
-       
-    def insert_fault(self, lap, byte_attacked, byte_value):
+    
+    def resetSolver(self):
+        s = Solver()
+        self.aes1.reset()
+        self.aes2.reset()
+        return s
+
+    def reset(self):
+        self.s = DFA.resetSolver(self)
+
+    def insert(self, lap, byte_attacked, byte_value):
         state1 = self.aes1.cipher[lap]
         state2 = self.aes2.cipher[lap]
         
-        # Insert fault on byte 11
+        self.aes2.subByte(lap+1)
+
+        # Insert fault on byte
         self.fault = BitVec("fault", 8)
-        state2[byte_attacked//4][(byte_attacked)%4] = state2[byte_attacked//4][byte_attacked%4] ^ fault
-        self.aes2.cipher[lap] = state2
+        #state2[byte_attacked//4][(byte_attacked)%4] = state2[byte_attacked//4][byte_attacked%4] ^ self.fault
+        state2[byte_attacked//4][(byte_attacked)%4] = self.fault
+        self.aes2.cipher[lap+1] = state2
         
         # perform the rounds with the fault
         for l in range(lap+1, self.aes2.Nr):
-            self.aes2.subByte(l)
             self.aes2.mixColumn(l)
             self.aes2.addRoundKey(l)
         
@@ -29,10 +40,20 @@ class DFA():
         # Init the fault
         self.aes2.s.add(self.fault == byte_value)
 
-    def perform_fault(self, safe_m, faulted_m):
+    def exploit(self, safe_m, faulted_m):
+        self.aes1.addCipher(safe_m)
+        self.aes2.addCipher(faulted_m)
         for i in range(4):
             for j in range(4):
                 self.s.add(self.aes1.cipher[-1][i][j] ^ self.aes2.cipher[-1][i][j] == 
-                        int(safe_m[2*(i*4+j):2*(i*4+j+1)], 16) ^ int(faulted_m[2*(i*4+j):2*(i*4+j+1)], 16))
+                  int(safe_m[2*(i*4+j):2*(i*4+j+1)], 16) ^ int(faulted_m[2*(i*4+j):2*(i*4+j+1)], 16))
         
+        sat_status = self.s.check()
+        while(sat_status == sat):
+            if(sat_status == sat):
+                print(self.s.model().evaluate(self.aes1.keyRounds[10][0][0]))
+            else:
+                print("No Solution")
 
+            self.s.add()
+            sat_status = self.s.check()
