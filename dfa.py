413ca27a214b8eb8efcc882a7e97c96c8bd4e5eb
Brissouille <valliere.brice@gmail.com>
2019-07-16 22:31:55 +0200

    Development of the DFA

--- dfa.py
+++ dfa.py
@@ -8,39 +8,41 @@ class DFA():
         self.s = Solver()
     
     def resetSolver(self):
-        s = Solver()
-        aes1.reset()
-        aes2.reset()
-        return s
+        self.s.reset()
+        for aes in self.aes:
+            # reset safe and faulted aes
+            for i in aes:
+                i.reset()
+        return self.s
 
     def reset(self):
         self.s = DFA.resetSolver(self)
 
     def insert(self, lap, byte_attacked, byte_value):
-        aes1 = Aes(128, "m_%02d_1" %(len(self.aes)))
-        aes2 = Aes(128, "m_%02d_2" %(len(self.aes)))
+        aes1 = Aes(128, "m_%02d_s" %(len(self.aes)))
+        aes2 = Aes(128, "m_%02d_f" %(len(self.aes)))
         self.aes.append([aes1, aes2])
 
-        A = [[BitVec("A_%02d_%02d" % (j,i), 8) for i in range(4)] for j in range(4)] 
-        K9 = [[BitVec("K9_%02d_%02d" % (j,i), 8) for i in range(4)] for j in range(4)] 
-        K10 = [[BitVec("K10_%02d_%02d" % (j,i), 8) for i in range(4)] for j in range(4)] 
+        #A = [[BitVec("A_%02d_%02d" % (j,i), 8) for i in range(4)] for j in range(4)] 
+        #K9 = [[BitVec("K9_%02d_%02d" % (j,i), 8) for i in range(4)] for j in range(4)] 
+        #K10 = [[BitVec("K10_%02d_%02d" % (j,i), 8) for i in range(4)] for j in range(4)] 
 
         # We take the previous state
-        #aes2.cipher[lap] = deepcopy(aes2.cipher[lap-1])
-        aes2.cipher[lap] = deepcopy(A)
+        aes2.cipher[lap] = deepcopy(aes2.cipher[lap-1])
+        #aes2.cipher[lap] = deepcopy(A)
         
-        aes2.keyRounds[9] = deepcopy(K9)
-        aes2.keyRounds[10] = deepcopy(K10)
+        #aes2.keyRounds[9] = deepcopy(K9)
+        #aes2.keyRounds[10] = deepcopy(K10)
 
         # Perform subByte 
         aes2.subByte(lap)
 
         # Insert fault on byte previous MixColumn
         state2 = aes2.cipher[lap]
-        self.fault = BitVec("fault", 8)
-        #state2[byte_attacked//4][(byte_attacked)%4] = self.fault
-        state2[byte_attacked//4][(byte_attacked)%4] = state2[byte_attacked//4][byte_attacked%4] ^ self.fault 
-       
+        fault = BitVec("fault", 8)
+        #state2[byte_attacked//4][(byte_attacked)%4] = fault
+        state2[byte_attacked//4][(byte_attacked)%4] = state2[byte_attacked//4][byte_attacked%4] ^ fault 
+        
         # Perform the rounds 9 with the fault
         for l in range(lap, aes2.Nr):
             aes2.mixColumn(l)
@@ -52,17 +54,21 @@ class DFA():
         aes2.addRoundKey(aes2.Nr)
 
         # Init the fault
-        self.s.add(self.fault == byte_value)
+        #self.s.add(fault == byte_value)
+        aes2.s.add(fault == byte_value)
 
     def exploit(self, safe_m, faulted_m):
-        aes1, aes2 = self.aes[0]
-        aes1.addCipher(safe_m)
-        aes2.addCipher(faulted_m)
-        print(aes2.cipher[-1][0][0])
-        input()
-        self.s.add(aes1.s.assertions())
-        self.s.add(aes2.s.assertions())
+        # We agregate the solver into one solver
+        for aes in self.aes:
+            aes1, aes2 = aes
+            aes1.addCipher(safe_m)
+            aes2.addCipher(faulted_m)
+            print(aes2.cipher[-1][0][0])
+            input()
+            self.s.add(aes1.s.assertions())
+            self.s.add(aes2.s.assertions())
         
+        # Resolution of the equation
         sat_status = self.s.check()
         if(sat_status == sat):
             print("Solution")
